7dd7cbc8a4692ad4cdad18f1c91ea21b55e07975
ChristianGroeber <swiss8oy.chg@gmail.com>
2024-12-14 13:53:11 +0100

    first try counting corners, but it's missing just a few

--- src/day12.c
+++ src/day12.c
@@ -11,6 +11,7 @@ typedef struct Area {
     long num_sides;
     int start_x;
     int start_y;
+    PointArray *corners;
 } Area;
 
 typedef struct AreaList {
@@ -21,6 +22,7 @@ typedef struct AreaList {
 
 void free_area_list(AreaList *areas) {
     for (int i = 0; i < areas->length; i++) {
+        free_point_array(areas->areas[i]->corners);
         free(areas->areas[i]);
     }
     free(areas->areas);
@@ -65,27 +67,27 @@ long calculate_price_fences(AreaList *list) {
 
 void count_sides_recursive(Area *area, IntMatrix *map, IntMatrix *visited, int cur_x, int cur_y) {
     visited->data[cur_y + 1][cur_x + 1] = 1;
-    
+
     // Check all adjacent positions
     int directions[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // right, down, left, up
-    
+
     for (int i = 0; i < 4; i++) {
         int next_x = cur_x + directions[i][0];
         int next_y = cur_y + directions[i][1];
         char next_char = get_character(map, next_y, next_x);
-        
+
         // printf("next_x = %d, next_y = %d\n", next_x, next_y);
         // If we find our shape's character, count it as a side
         if (next_char == area->identifier) {
             area->num_sides++;
-            
+
             // Look for the next position to move to
             for (int j = 0; j < 4; j++) {
                 if (j != (i + 2) % 4) {  // Don't go backwards
                     int try_x = cur_x + directions[j][0];
                     int try_y = cur_y + directions[j][1];
                     char try_char = get_character(map, try_y, try_x);
-                    
+
                     // Move to any position that isn't our shape's character and hasn't been visited
                     if (try_char != area->identifier && visited->data[try_y + 1][try_x + 1] != 1) {
                         count_sides_recursive(area, map, visited, try_x, try_y);
@@ -116,7 +118,7 @@ void count_sides(Area *area, IntMatrix *map) {
         area->num_sides = 4;
         return;
     }
-    
+
     // Initialize visited matrix
     IntMatrix *visited = init_int_matrix(map->rows + 2, map->cols + 2);
     for (int i = 0; i < visited->rows; i++) {
@@ -124,21 +126,21 @@ void count_sides(Area *area, IntMatrix *map) {
             visited->data[i][j] = 0;
         }
     }
-    
+
     area->num_sides = 0;
-    
+
     // Find an empty space adjacent to our starting position
     int directions[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     for (int i = 0; i < 4; i++) {
         int start_x = area->start_x + directions[i][0];
         int start_y = area->start_y + directions[i][1];
-        
+
         if (get_character(map, start_y, start_x) != area->identifier) {
             count_sides_recursive(area, map, visited, start_x, start_y);
             break;
         }
     }
-    
+
     printf("Visited for %c\n", area->identifier);
     print_matrix(visited);
     free_matrix(visited);
@@ -154,38 +156,40 @@ long calculate_price_sides(AreaList *list) {
     return price;
 }
 
-IntMatrix *extract_borders(IntMatrix *map) {
-    IntMatrix *out = init_int_matrix(map->rows, map->cols);
-    char *compare = calloc(sizeof(char), 5);
-    char *expect = calloc(sizeof(char), 5);
-    for (int i = 0; i < map->rows; i++) {
-        for (int j = 0; j < map->cols; j++) {
-            char *s = malloc(sizeof(char) * 2);
-            char c = map->data[i][j];
-            sprintf(s, "%c", c);
-
-            char u = get_character(map, i - 1, j);
-            char d = get_character(map, i + 1, j);
-            char l = get_character(map, i, j - 1);
-            char r = get_character(map, i, j + 1);
-
-            int required_matches = 4;
-
-            sprintf(compare, "%c%c%c%c", u, d, l, r);
-            sprintf(expect, "%c%c%c%c", c, c, c, c);
-            int difference = 0;
-            for (int x = 0; x < 4; x++) if (compare[x] != expect[x]) difference++;
-
-            if (4 - difference < required_matches) {
-                out->data[i][j] = c;
-                int cur_fence_count = 0;
-            } else {
-                out->data[i][j] = '.';
-            }
+int count_corners(IntMatrix *map, int x, int y, Area *area) {
+    int directions[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int corners[] = {0, 0, 0, 0};
+    char *neighbors = malloc(sizeof(char) * 5);
+    sprintf(neighbors, "....");
+    int adjacent_neighbors = 0;
+    for (int i = 0; i < 4; i++) {
+        int nx = x + directions[i][0];
+        int ny = y + directions[i][1];
+        char neighbor = get_character(map, ny, nx);
+        neighbors[i] = neighbor;
+        if (neighbor == area->identifier) {
+            adjacent_neighbors++;
         }
     }
 
-    return out;
+    int sides;
+    // we're inside a line
+    if (neighbors[0] == area->identifier && neighbors[2] == area->identifier || neighbors[1] == area->identifier && neighbors[3] == area->identifier) {
+        sides = 0;
+    } else {
+        if (adjacent_neighbors == 0) {
+            sides = 4;
+        } else if (adjacent_neighbors == 1) {
+            sides = 2;
+        } else if (adjacent_neighbors == 2) {
+            sides = 1;
+        } else if (adjacent_neighbors == 3) {
+            sides = 1;
+        }
+    }
+    printf("x: %d, y: %d\t%s\tsides: %d\n", x, y, neighbors, sides);
+    free(neighbors);
+    return sides;
 }
 
 void walk_area_recursive(IntMatrix *map, IntMatrix *visited, int x, int y, Area *area) {
@@ -211,6 +215,8 @@ void walk_area_recursive(IntMatrix *map, IntMatrix *visited, int x, int y, Area 
     visited->data[y][x] = 1;
     area->area_size++;
 
+    area->num_sides += count_corners(map, x, y, area);
+
     walk_area_recursive(map, visited, x - 1, y, area);
     walk_area_recursive(map, visited, x + 1, y, area);
     walk_area_recursive(map, visited, x, y - 1, area);
@@ -225,6 +231,7 @@ Area *walk_area(IntMatrix *map, IntMatrix *visited, int x, int y) {
     area->num_sides = 0;
     area->start_x = x;
     area->start_y = y;
+    area->corners = init_point_array(10);
     walk_area_recursive(map, visited, x, y, area);
 
     return area;
@@ -253,7 +260,7 @@ IntMatrix *read_map(FILE *file) {
         for (int j = 0; j < cols; j++) {
             if (visited->data[i][j] == 0) {
                 Area *area = walk_area(map, visited, j, i);
-                printf("Area %c of size %ld with %ld fences\n", area->identifier, area->area_size, area->fence_count);
+                printf("Area %c of size %ld with %ld fences and %ld sides\n", area->identifier, area->area_size, area->fence_count, area->num_sides);
                 area_list_append(areas, area);
             }
         }
@@ -261,11 +268,11 @@ IntMatrix *read_map(FILE *file) {
 
     timer_stop(t1, perf);
 
-    IntMatrix *borders = extract_borders(map);
-    print_matrix_as_char(borders);
-    for (int i = 0; i < areas->length; i++) {
-        count_sides(areas->areas[i], borders);
-    }
+    // IntMatrix *borders = extract_borders(map);
+    // print_matrix_as_char(borders);
+    // for (int i = 0; i < areas->length; i++) {
+    //     count_sides(areas->areas[i], borders);
+    // }
 
     timer_start("Calculation", perf);
     printf("Part 1: %ld\n", calculate_price_fences(areas));
