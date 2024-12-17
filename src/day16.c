b154e0724664e730938b1f7843c1118202a1214d
ChristianGroeber <swiss8oy.chg@gmail.com>
2024-12-17 11:35:55 +0100

    performance improvements by ignoring wall tiles

--- src/day16.c
+++ src/day16.c
@@ -8,9 +8,8 @@
 
 /*
 ----------------------------
-Search Paths: 47.5  s
-Count Tiles:  42.41 ms
-Total:        47.5  s
+Search Paths: 12.9  s
+Count Tiles:  13    ms
 ----------------------------
 */
 
@@ -123,23 +122,30 @@ void print_path(IntMatrix *map, PathNode *root) {
 }
 
 PathNode *find_path(IntMatrix *map, IntMatrix *visited, PathNode *previous, Point *direction, Point *target, int current_score, int *lowest_score) {
+    int new_y = previous->y + (int) direction->y;
+    int new_x = previous->x + (int) direction->x;
+
+    char cur = map->data[new_y][new_x];
+
+    if (cur == '#') {
+        return NULL;
+    }
+
     PathNode *current = malloc(sizeof(PathNode));
     current->prev = previous;
-    current->x = previous->x + direction->x;
-    current->y = previous->y + direction->y;
+    current->x = new_x;
+    current->y = new_y;
     current->value = current_score;
     current->o = NULL;
     current->l = NULL;
     current->r = NULL;
     current->reaches_target = false;
-    char cur = map->data[current->y][current->x];
-    int previous_visit = visited->data[(int) current->y][(int) current->x];
 
+    int previous_visit = visited->data[(int) current->y][(int) current->x];
     if (previous_visit + 1000 < current_score && previous_visit != 0) {
         // Were already here with lower score
         return current;
-    } 
-    if (target->x == current->x && target->y == current->y) {
+    } else if (target->x == current->x && target->y == current->y) {
         // Found Target
         *lowest_score = current_score;
         current->reaches_target = true;
@@ -152,9 +158,6 @@ PathNode *find_path(IntMatrix *map, IntMatrix *visited, PathNode *previous, Poin
         return current;
     } else if (find_parent_node(previous, current->x, current->y) != NULL && previous->x != current->x && previous->y != current->y) {
         // We were already here in the current path
-        return current;
-    } else if (cur == '#') {
-        current->value = INT_MAX;
         return current;
     }
 
@@ -179,7 +182,7 @@ PathNode *find_path(IntMatrix *map, IntMatrix *visited, PathNode *previous, Poin
     current->l = find_path(map, visited, current, l, target, current_score + 1001, lowest_score);
     current->r = find_path(map, visited, current, r, target, current_score + 1001, lowest_score);
 
-    if (current->o->reaches_target || current->r->reaches_target || current->l->reaches_target) {
+    if ((current->o != NULL && current->o->reaches_target) || (current->r != NULL && current->r->reaches_target) || (current->l != NULL && current->l->reaches_target)) {
         current->reaches_target = true;
     }
 
@@ -274,7 +277,11 @@ int get_lowest_score(PathNode *node) {
         }
     }
 
-    if ((node->l == NULL || !node->l->reaches_target) && (node->o == NULL || !node->o->reaches_target) && (node->r == NULL || !node->r->reaches_target)) {
+    if (
+        (node->l == NULL || !node->l->reaches_target) 
+        && (node->o == NULL || !node->o->reaches_target)
+        && (node->r == NULL || !node->r->reaches_target)
+    ) {
         return node->value;
     }
 
